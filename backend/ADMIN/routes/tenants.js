a8d45eb3ce9b331f7fdb7be082cbd2c045547a16
Mohammadalomari04 <ke7osh@gmail.com>
2026-03-07 16:38:52 +0300

    s

--- backend/ADMIN/routes/tenants.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createTenant } from "../controllers/createTenant.js";
-
-const router = express.Router();
-
-router.post("/create", createTenant);
-
-export default router;
